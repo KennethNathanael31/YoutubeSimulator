cc78d219de15e466864024f3895f3cf687e4599a
KennethNathanael3 <kennethnathanael3105@gmail.com>
2025-12-06 23:04:24 +0700

    Undang Anggota untuk Kanal Grup

--- Youtube/src/User.java
+++ Youtube/src/User.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class User {
-    //akses
+    //invite
     private int idPengguna;
     protected String email;
     protected String namaPengguna;
