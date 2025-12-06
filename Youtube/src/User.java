e0ac15ede7db6bb91cfdfe220799aa83f152ce71
KennethNathanael3 <kennethnathanael3105@gmail.com>
2025-12-06 22:54:43 +0700

    Fitr list suscribe ke kanal (pengguna)

--- Youtube/src/User.java
+++ Youtube/src/User.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class User {
-    //wwwwww
+    //wwwwwwwwwwwwwwwwwww
     private int idPengguna;
     protected String email;
     protected String namaPengguna;
