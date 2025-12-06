1d74fc15b9981c9cf4b0fd972dca72220c8d416a
KennethNathanael3 <kennethnathanael3105@gmail.com>
2025-12-06 23:12:18 +0700

    Unit Test 4

--- Youtube/src/Channel.java
+++ Youtube/src/Channel.java
@@ -1,4 +1,3 @@
-// Kelas Kanal 1.8
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -6,8 +5,8 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.*;
 
-public abstract class Channel { ////aaaaaaaa
-    private int idKanal; //aaa
+public abstract class Channel {
+    private int idKanal;
     private int idPengguna;
     private Date tanggalPembuatanKanal;
     protected String namaKanal;
