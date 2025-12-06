fdde6367d5ff5f474feacdaea45f7bb0332c3376
Coretryxx <cleon.adam88@gmail.com>
2025-12-06 23:07:59 +0700

    Hapus Anggota

--- Youtube/src/Channel.java
+++ Youtube/src/Channel.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.*;
 
-public abstract class Channel { ////aaaaa
+public abstract class Channel { ////aaaaaaaa
     private int idKanal; //aaa
     private int idPengguna;
     private Date tanggalPembuatanKanal;
