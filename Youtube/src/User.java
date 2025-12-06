5b2ceb649e4fa13ed73e9072d0f8113c724ba97e
KennethNathanael3 <kennethnathanael3105@gmail.com>
2025-12-06 22:24:04 +0700

    Pembuatan fitur login Pengguna dan Admin

--- Youtube/src/User.java
+++ Youtube/src/User.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class User {
-    //Transformasi
+    //aadada
     private int idPengguna;
     protected String email;
     protected String namaPengguna;
