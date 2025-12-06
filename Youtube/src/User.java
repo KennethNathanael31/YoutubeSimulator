d3c6b77e5c794177f099a506bc0023a004078f69
KennethNathanael3 <kennethnathanael3105@gmail.com>
2025-12-06 22:19:54 +0700

    transformasi ERD ke relasional

--- Youtube/src/User.java
+++ Youtube/src/User.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class User {
-    //SRS
+    //Transformasi
     private int idPengguna;
     protected String email;
     protected String namaPengguna;
