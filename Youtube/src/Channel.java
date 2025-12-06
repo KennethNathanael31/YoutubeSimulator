129d3ec6fdf8742da5361aa64fa375e240750572
Coretryxx <cleon.adam88@gmail.com>
2025-12-06 22:48:38 +0700

    fitur dislike

--- Youtube/src/Channel.java
+++ Youtube/src/Channel.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.util.*;
 
 public abstract class Channel { ////aaaaaaaa
-    private int idKanal;
+    private int idKanal; ///id
     private int idPengguna;
     private Date tanggalPembuatanKanal;
     protected String namaKanal;
